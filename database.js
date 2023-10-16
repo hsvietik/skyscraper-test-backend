bb681426ff5d88f986572cd8585b1d3558611361
Hanna Svietik <hsvietik@gmail.com>
2023-10-16 16:18:19 +0100

    connection settings changed

--- database.js
+++ database.js
@@ -2,12 +2,23 @@ const mysql = require("mysql2");
 
 const { MYSQL_PASSWORD } = process.env;
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: MYSQL_PASSWORD,
+//   database: "skyscrappers",
+//   port: 3306,
+// });
+
+// module.exports = connection;
+
+const connection = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
   password: MYSQL_PASSWORD,
   database: "skyscrappers",
   port: 3306,
+  // connectionLimit: 10, // Adjust as needed
 });
 
 module.exports = connection;
